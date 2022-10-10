1cd8d1ccdfb8402fbc621df9988e1b17d517f447
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-10 18:16:37 +0200

    aggiunta elementi (mi annoiavo)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const nomeUtente = prompt ('Quale è il tuo nome?');
 const cognomeUtente = prompt ('Quale è il tuo cognome?');
 const colorePreferito = prompt ('Quale è il tuo colore preferito?');
-const numeroCasuale = (22);
+const numeroCasuale = 22;
 let output;
 
 
-
+document.getElementById('accesso-effettuato').innerHTML= `accesso effettuato`
 output = `
 ${nomeUtente}${cognomeUtente}${colorePreferito}${numeroCasuale}
 `;

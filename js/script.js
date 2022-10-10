616dce61d63f57f53a94a9c1b71d8ddd50621cf5
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-10 14:15:44 +0200

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const nomeUtente = prompt ('quale è il tuo nome?');
 const cognomeUtente = prompt ('quale è il tuo cognome?');
 const colorePreferito = prompt ('quale è il tuo colore preferito?');
+const numeroCasuale = (22);
 let output;
 
 
 
 output = `
-${nomeUtente}${cognomeUtente}${colorePreferito}
+${nomeUtente}${cognomeUtente}${colorePreferito}${numeroCasuale}
 `;
 document.getElementById('saluto').innerHTML += output;

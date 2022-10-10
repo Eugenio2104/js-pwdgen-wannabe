46438a7cd2acf80ad0ef3d45bc557d79d5907113
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-10 14:17:42 +0200

    correzione maiuscole su domande prompt

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const nomeUtente = prompt ('quale è il tuo nome?');
-const cognomeUtente = prompt ('quale è il tuo cognome?');
-const colorePreferito = prompt ('quale è il tuo colore preferito?');
+const nomeUtente = prompt ('Quale è il tuo nome?');
+const cognomeUtente = prompt ('Quale è il tuo cognome?');
+const colorePreferito = prompt ('Quale è il tuo colore preferito?');
 const numeroCasuale = (22);
 let output;
 

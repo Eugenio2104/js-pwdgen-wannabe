a91c23b6676d20a350d316cdab87427b8a66ed26
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-11 17:00:43 +0200

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,24 +1,13 @@
-const nomeCognome = prompt("Inserisci nome e cognome");
-const kmDaPercorrere = parseInt(prompt("Quanti km vuoi percorrere?"));
-const etaPasseggero = parseInt(prompt("Quanti anni hai?")); 
-const prezzoKm = 0.21;
-let sconto = 0;
-const prezzoBiglietto = (kmDaPercorrere * prezzoKm)
-let scontoApplicato;
-let prezzoScontato;
-let prezzoFinale;
+const nomeUtente = prompt ('Quale è il tuo nome?');
+const cognomeUtente = prompt ('Quale è il tuo cognome?');
+const colorePreferito = prompt ('Quale è il tuo colore preferito?');
+const numeroCasuale = (22);
+const numeroCasuale = 22;
+let output;
 
-prezzoFinale = prezzoBiglietto;
 
-if(etaPasseggero < 18){
-  sconto = 0.2;
-  prezzoFinale = prezzoBiglietto - (prezzoBiglietto * sconto);
-}
 
-if(etaPasseggero > 65) {
-  sconto = 0.4;
-    prezzoFinale = prezzoBiglietto - (prezzoBiglietto * sconto);
-
-}
-
-console.log('Il prezzo del biglietto è ',prezzoFinale)
+document.getElementById('accesso-effettuato').innerHTML= `accesso effettuato`
+output = `
+${nomeUtente}${cognomeUtente}${colorePreferito}${numeroCasuale}
+`;
